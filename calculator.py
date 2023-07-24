86f098c994e9334ed752bafbe8ad98e009b7b088
AlenBase <135118395+AlenBase@users.noreply.github.com>
2023-07-24 17:08:21 +0400

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,16 +1,29 @@
 while True:
-    number1=int(input('Write First Number:\n'))
-    operator=input('Write Operator(+,-,/,*)\n')
-    number2=int(input('Write Second Number:\n'))
-    if operator == "+":
-        result = number1 + number2
-    if operator == "-":
-        result = number1 - number2
-    if operator == "*":
-        result = number1 * number2
-    if operator == "/":
-        result = number1 / number2
-    print(result)
-    choise = input('You want to quit?(y/n)')
-    if choise == 'y':
-        break
+    try:
+        number1=int(input('Write First Number:\n'))
+        operator=input('Write Operator(+,-,/,*)\n')
+        number2=int(input('Write Second Number:\n'))
+        if operator == "+":
+            result = number1 + number2
+        if operator == "-":
+            result = number1 - number2
+        if operator == "*":
+            result = number1 * number2
+        if operator == "/":
+            result = number1 / number2
+        print(result)
+        choise = input('You want to quit?(y/n)')
+        if choise == 'y':
+            break
+    
+    except ValueError:
+        print('Write only int type numbers')
+        choise = input('You want to quit?(y/n)')
+        if choise == 'y':
+            break
+        
+    except ZeroDivisionError:
+        print("Can't divide by zero")
+        choise = input('You want to quit?(y/n)')
+        if choise == 'y':
+            break
